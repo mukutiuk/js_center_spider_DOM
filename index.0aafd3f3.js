bce8b87aec0da727441e52f98e3a8d1d4938ff61
Roma <roma150269@gmail.com>
2024-06-06 15:14:01 +0300

    make build

---
+++ index.0aafd3f3.js
@@ -0,0 +1,2 @@
+var t=document.querySelector("img");t.style.top="50%",t.style.left="50%",t.style.top="50%",t.style.transform="translate(-50%, -50%)";
+//# sourceMappingURL=index.0aafd3f3.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,8 @@
+"use strict";
+const spider = document.querySelector("img");
+spider.style.top = "50%";
+spider.style.left = "50%";
+spider.style.top = "50%";
+spider.style.transform = "translate(-50%, -50%)";
+
+//# sourceMappingURL=index.f75de5e1.js.map
